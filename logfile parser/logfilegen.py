2b8eefcd00602011647b3e354106e489b1564791
Sam Mancill <cromambo@gmail.com>
2013-05-27 13:48:42 -0700

    now varies # of elements and output file name based on command line args. fixed hours starting with 24

--- logfile parser/logfilegen.py
+++ logfile parser/logfilegen.py
@@ -1,15 +1,22 @@
 
 import random
 from random import randint
+import sys
 
 def main():
+  if len(sys.argv) != 3:
+    print ('usage logfilegen.py outputfilename.txt 5000 #number of lines in output file')
+    sys.exit(-1)
+  
+  outputfilename = sys.argv[1]
+  elements = int(sys.argv[2])
+  
   random.seed()
-  with open('giantlog.txt', 'w') as outfile:
-    elements = 1000000
+  with open(outputfilename, 'w') as outfile:
     while elements > 0:
       outfile.write('ip, ip, no:%(number)d ' % {'number':randint(0, 1000000)}),
       outfile.write ('%(hour)02d:%(min)02d:%(sec)02d' % {'hour':randint(0,23), 'min':randint(0,59), 'sec':randint(0,59)}), 
-      outfile.write (' %(hour)02d:%(min)02d:%(sec)02d\n' % {'hour':randint(0,24), 'min':randint(0,59), 'sec':randint(0,59)}),
+      outfile.write (' %(hour)02d:%(min)02d:%(sec)02d\n' % {'hour':randint(0,23), 'min':randint(0,59), 'sec':randint(0,59)}),
       elements -= 1
 
 if __name__ == '__main__':
