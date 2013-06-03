0b68b8ea1f2e0b862eccbef607dd7c1e7548a898
Sam Mancill <cromambo@gmail.com>
2013-06-03 16:50:25 -0700

    changed to used a lambda function as sort key

--- basic/list1.py
+++ basic/list1.py
@@ -54,10 +54,7 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-  return sorted(tuples, key=tupleLast)
-
-def tupleLast(tuple):
-  return tuple[-1]
+  return sorted(tuples, key=lambda tuple: tuple[-1])
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
