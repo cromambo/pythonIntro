df258f972fc7290c6af2ee9393d60117d41ff3e3
Sam Mancill <cromambo@gmail.com>
2013-06-03 12:27:13 -0700

    finished logpuzzle

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -18,15 +18,21 @@ Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
-
 def read_urls(filename):
   """Returns a list of the puzzle urls from the given log file,
   extracting the hostname from the filename itself.
   Screens out duplicate urls and returns the urls sorted into
   increasing order."""
   # +++your code here+++
+  print (filename)
+  with open(filename) as file:
+    filecontents = file.read()
+    urls = set(re.findall('GET (\S*puzzle\S*)', filecontents))
+    underscore_index = filename.index('_')
+    host = filename[underscore_index + 1:]
+    return ['http://'+host+path for path in sorted(urls)]
+  return []
   
-
 def download_images(img_urls, dest_dir):
   """Given the urls already in the correct order, downloads
   each image into the given directory.
@@ -35,14 +41,38 @@ def download_images(img_urls, dest_dir):
   with an img tag to show each local image file.
   Creates the directory if necessary.
   """
-  # +++your code here+++
+  create_directory(dest_dir)
   
+  index_file = open(dest_dir + '\index.html', 'w')
+  
+  index_file.write('<verbatim><html><body>')
+  count = 0
+  for url in img_urls:
+    print 'Retrieving:', url
+    destination_path = dest_dir + '\img' + str(count)+'.jpg'
+    index_file.write('<img src=%s>' % destination_path)
+    print 'Target:', destination_path
+    try:
+      urllib.urlretrieve(url, destination_path)
+    except:
+      print ('URL opening failed')
+    count += 1
+  index_file.write('</html></body>')
+  index_file.close()
+  
+def create_directory(dir):
+  # os.makedirs(dir, exist_ok=True) #python 3.2+only
+  if len(dir) != 0:
+    if not os.path.exists(dir):
+      print ('Creating', dir)
+      os.makedirs(dir)
+  pass
 
 def main():
   args = sys.argv[1:]
 
   if not args:
-    print 'usage: [--todir dir] logfile '
+    print ('usage: [--todir dir] logfile ')
     sys.exit(1)
 
   todir = ''
@@ -54,8 +84,9 @@ def main():
 
   if todir:
     download_images(img_urls, todir)
+    pass
   else:
-    print '\n'.join(img_urls)
+    print ('\n'.join(img_urls))
 
 if __name__ == '__main__':
   main()
