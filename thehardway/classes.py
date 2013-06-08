49e90df3df909357c54d253d315403191976ba53
cromambo <cromambo@gmail.com>
2013-06-07 19:18:59 -0700

    song class

--- thehardway/classes.py
+++ thehardway/classes.py
@@ -1,5 +1,13 @@
+class Song():
+  def __init__(self, lyrics):
+    self.lyrics = lyrics
+  def sing(self):
+    for line in self.lyrics:
+      print (line)
+      
 def main():
-  pass
+  happybirthday = Song("Happy birthday to you.")
+  happybirthday.sing()
 
 if __name__ == '__main__':
   main()  
