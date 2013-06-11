7115ef51fa6edd88b9d889c4f29ea5e5300627ad
cromambo <cromambo@gmail.com>
2013-06-10 21:32:46 -0700

    added game engine skeleton classes

--- thehardway/classes.py
+++ thehardway/classes.py
@@ -1,13 +1,42 @@
-class Song():
-  def __init__(self, lyrics):
-    self.lyrics = lyrics
-  def sing(self):
-    for line in self.lyrics:
-      print (line)
+class Scene(object):
+  def enter(self):
+    pass
+    
+class Engine(object):
+  def __init__(self, sceneMap):
+    pass
+  def play(self):
+    pass
+    
+class Death(Scene):
+  def enter(self):
+    pass
+class CentralCorridor(Scene):
+  def enter(self):
+    pass
+class LaserWeaponArmory(Scene):
+  def enter(self):
+    pass
+class TheBridge(Scene):
+  def enter(self):
+    pass
+class EscapePod(Scene):
+  def enter(self):
+    pass
+   
+class Map(object):
+  def __init__(self, start_scene):
+    pass
+   
+  def nextScene(self, sceneName):
+    pass
+  def openingScene(self):
+    pass
       
 def main():
-  happybirthday = Song("Happy birthday to you.")
-  happybirthday.sing()
+  gameMap = Map('Central Corridor')
+  gameEngine = Engine(gameMap)
+  gameEngine.play()
 
 if __name__ == '__main__':
   main()  
